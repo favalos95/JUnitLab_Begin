89619a3ae8a9fe7661d5fb8253ed631875235914
Francisco Avalos <franciscoavalos@Franciscos-MacBook-Pro.local>
2021-03-14 20:47:51 -0700

    //We added three test cases to the java file

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,14 @@
+package csc131.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GiftCardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
